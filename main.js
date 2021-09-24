302aa73621997c7b6335cce2b1f25b7ad4f05098
CesarMtzV <mc.cesar.art@gmail.com>
2021-09-24 07:23:25 -0500

    Se añadieron los primeros archivos para el proyecto

---
+++ main.js
@@ -0,0 +1,17 @@
+function main() {
+    const canvas = document.querySelector("#glCanvas");
+    const gl = canvas.getContext("webgl");
+  
+    if (gl === null) {
+      alert("El buscador no soporta WebGL");
+      return;
+    }
+  
+    // Set clear color to black, fully opaque
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // Clear the color buffer with specified clear color
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
+  
+window.onload = main;
+  
