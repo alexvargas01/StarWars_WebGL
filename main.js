666de595b5e64c351a7b044fa1e8b02d92cea29b
CesarMtzV <mc.cesar.art@gmail.com>
2021-09-28 19:12:46 -0500

    Se agregaron comentarios al código

--- main.js
+++ main.js
@@ -27,9 +27,11 @@ class BasicWorldDemo {
   }
 
   _Initialize() {
+    // Inicializar el Renderer de WebGL
     this._threejs = new THREE.WebGLRenderer({
       antialias: true,
     });
+    // Iniciar parámetros para el renderer
     this._threejs.shadowMap.enabled = true;
     this._threejs.shadowMap.type = THREE.PCFSoftShadowMap;
     this._threejs.setPixelRatio(window.devicePixelRatio);
@@ -41,6 +43,7 @@ class BasicWorldDemo {
       this._OnWindowResize();
     }, false);
 
+    // Creación de la cámara
     const fov = 60;
     const aspect = 1920 / 1080;
     const near = 1.0;
@@ -48,8 +51,10 @@ class BasicWorldDemo {
     this._camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
     this._camera.position.set(75, 20, 0);
 
+    // Crear la escena para los objetos
     this._scene = new THREE.Scene();
 
+    // Crear la luz y luz ambiental
     let light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
     light.position.set(20, 100, 10);
     light.target.position.set(0, 0, 0);
@@ -67,14 +72,15 @@ class BasicWorldDemo {
     light.shadow.camera.bottom = -100;
     this._scene.add(light);
 
+    // Añadir la luz a la escena
     light = new THREE.AmbientLight(0x101010);
     this._scene.add(light);
 
-    const controls = new OrbitControls(
-      this._camera, this._threejs.domElement);
+    const controls = new OrbitControls(this._camera, this._threejs.domElement);
     controls.target.set(0, 5, 0);
     controls.update();
 
+    // Cargar el cubo de fondo
     const loader = new THREE.CubeTextureLoader();
     const texture = loader.load([
         './skybox/posx.png',
@@ -86,6 +92,7 @@ class BasicWorldDemo {
     ]);
     this._scene.background = texture;
 
+    // Crear un plano
     const plane = new THREE.Mesh(
         new THREE.PlaneGeometry(100, 100, 10, 10),
         new THREE.MeshStandardMaterial({
@@ -94,8 +101,10 @@ class BasicWorldDemo {
     plane.castShadow = false;
     plane.receiveShadow = true;
     plane.rotation.x = -Math.PI / 2;
+    // Agregar el plano a la esfera
     this._scene.add(plane);
 
+    // Crear la esfera
     const s1 = new THREE.Mesh(
       new THREE.SphereGeometry(2,32,32),
       new THREE.ShaderMaterial({
@@ -106,6 +115,7 @@ class BasicWorldDemo {
     )
     s1.position.set(0,5,0);
     s1.castShadow = true;
+    // Agregar la esfera a la escena
     this._scene.add(s1);
     this._sphere = s1;
 
@@ -118,6 +128,8 @@ class BasicWorldDemo {
     this._threejs.setSize(window.innerWidth, window.innerHeight);
   }
 
+  // Request Animation Frame
+  // Ejecutar esta funcion por cada frame de la pantalla
   _RAF() {
     requestAnimationFrame(() => {
       this._threejs.render(this._scene, this._camera);
@@ -132,12 +144,3 @@ let _APP = null;
 window.addEventListener('DOMContentLoaded', () => {
   _APP = new BasicWorldDemo();
 });
-
-/*
-'./resources/posx.jpg',
-        './resources/negx.jpg',
-        './resources/posy.jpg',
-        './resources/negy.jpg',
-        './resources/posz.jpg',
-        './resources/negz.jpg',
-*/
