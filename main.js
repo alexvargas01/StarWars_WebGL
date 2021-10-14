5253c0743c6fda7230d763e2d7952afac02383f1
CesarMtzV <mc.cesar.art@gmail.com>
2021-10-14 14:34:45 -0500

    Added promised based model loading

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const sizes = {
 }
 const fov = 75;
 const near = 0.1;
-const far = 1000;
+const far = 10000;
 const camera = new THREE.PerspectiveCamera( fov, sizes.width/sizes.height, near, far );
 camera.position.set(60, 80, 50);
 
@@ -22,22 +22,6 @@ camera.position.set(60, 80, 50);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( sizes.width, sizes.height );
 document.body.appendChild( renderer.domElement );
-
-
-// <----- LOADER PARA EL MODELO ----->
-// TODO: Agregar más naves imperiales y rebeldes
-const loader = new GLTFLoader();
-loader.load( './assets/star_wars_imperial_ii_star_destroyer/scene.gltf', function ( gltf ) {
-
-	const zelda = gltf.scene.children[0];
-	zelda.scale.set(0.3,0.3,0.3);
-	scene.add( gltf.scene );
-
-}, undefined, function ( error ) {
-
-	console.error( error );
-
-} );
 
 
 // <----- LUZ DIRECCIONAL ----->
@@ -66,6 +50,55 @@ const texture = background_loader.load([
 ])
 scene.background = texture;
 
+// <----- LOADER PARA EL MODELO ----->
+// TODO: Agregar más naves imperiales y rebeldes
+// const loader = new GLTFLoader();
+// loader.load( './assets/star_wars_imperial_ii_star_destroyer/scene.gltf', function ( gltf ) {
+
+// 	const zelda = gltf.scene.children[0];
+// 	zelda.scale.set(0.3,0.3,0.3);
+// 	scene.add( gltf.scene );
+
+// }, undefined, function ( error ) {
+
+// 	console.error( error );
+
+// } );
+
+function loadModel(url){
+	return new Promise(resolve => {
+		new GLTFLoader().load(url, resolve);
+	})
+}
+
+// Lista de modelos
+// TODO: Agregar los modelos de los soles, y tatooine
+let rebel_frigate, star_destroyer, tie_fighter, x_wing;
+
+let p1 = loadModel('./assets/star_wars_imperial_ii_star_destroyer/scene.gltf').then(result => {star_destroyer = result.scene.children[0]; });
+let p2 = loadModel('./assets/nebulon_b_frigate/scene.gltf').then(result => {rebel_frigate = result.scene.children[0]; });
+let p3 = loadModel('./assets/tie_fighter/scene.gltf').then(result => {tie_fighter = result.scene.children[0]; });
+let p4 = loadModel('./assets/x-wing_fighter/scene.gltf').then( result => {x_wing = result.scene.children[0]; });
+
+Promise.all([p1, p2, p3, p4]).then( () => {
+
+	// Posición de los modelos
+	star_destroyer.position.set(0, 0, 0);
+	rebel_frigate.position.set(500, 200, 0);
+
+	// Tamaño de los modelos
+	rebel_frigate.scale.set(18,18,18);
+
+	// Rotación inicial de los modelos
+	rebel_frigate.rotateZ(90);
+
+	// Agregar los modelos a la escena
+	scene.add(star_destroyer);
+	scene.add(rebel_frigate);
+
+	console.log("Entered promise");
+	animate();
+})
 
 // <----- RAF para animar todo ----->
 function animate() {
