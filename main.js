c45ce1920e18cbf3590ec7f934dea77c2e43b6fc
Alex Vargas <alexjesusvargas@gmail.com>
2021-10-28 13:23:52 -0500

    added tweenJS & fighter movement

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from "./three.js-master/build/three.module.js";
-import * as TWEEN from "./tween.js/dist/tween.umd.js";
+import TWEEN from 'https://cdn.jsdelivr.net/npm/@tweenjs/tween.js@18.5.0/dist/tween.esm.js';
 import { GLTFLoader } from "./three.js-master/examples/jsm/loaders/GLTFLoader.js";
 import { OrbitControls } from "./three.js-master/examples/jsm/controls/OrbitControls.js";
 
@@ -23,13 +23,13 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.set(-500, 300, 600);
 
 // <----- CREACIÓN DEL RENDERER ----->
-const renderer = new THREE.WebGLRenderer({antialias: true});
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(sizes.width, sizes.height);
 document.body.appendChild(renderer.domElement);
 
 // <----- LUZ AMBIENTAL ----->
-const ambientLight = new THREE.AmbientLight( 0x404040 ); // soft white light
-scene.add( ambientLight );
+const ambientLight = new THREE.AmbientLight(0x404040); // soft white light
+scene.add(ambientLight);
 
 // <----- LUZ DIRECCIONAL ----->
 const colorDeLuz = 0xffccaa;
@@ -42,8 +42,8 @@ const directionalLight2 = new THREE.DirectionalLight(
   colorDeLuz,
   intensidadDeLuz
 );
-directionalLight1.position.set(10000, 20000, -100000)
-directionalLight2.position.set(35000, 20000, -110000)
+directionalLight1.position.set(10000, 20000, -100000);
+directionalLight2.position.set(35000, 20000, -110000);
 scene.add(directionalLight1);
 scene.add(directionalLight2);
 
@@ -65,60 +65,107 @@ const texture = background_loader.load([
 scene.background = texture;
 
 // <----- LOADERS PARA LOS MODELOS ----->
-function loadModel(url){
-	return new Promise(resolve => {
-		new GLTFLoader().load(url, resolve);
-	})
+function loadModel(url) {
+  return new Promise((resolve) => {
+    new GLTFLoader().load(url, resolve);
+  });
 }
 
 // Lista de modelos
-let rebel_fleet, star_destroyer1, star_destroyer2, tie_fighter, x_wing, tatooine, sun1, sun2;
+let rebel_fleet,
+  star_destroyer1,
+  star_destroyer2,
+  tie_fighter,
+  x_wing,
+  tatooine,
+  sun1,
+  sun2;
 
-let p1 = loadModel('./assets/star_wars_imperial_ii_star_destroyer/scene.gltf').then(result => {star_destroyer1 = result.scene.children[0]; });
-let p2 = loadModel('./assets/resistance_fleet__resistance_navy/scene.gltf').then(result => {rebel_fleet = result.scene.children[0]; });
-let p3 = loadModel('./assets/tie_fighter/scene.gltf').then(result => {tie_fighter = result.scene.children[0]; });
-let p4 = loadModel('./assets/x-wing_fighter/scene.gltf').then( result => {x_wing = result.scene.children[0]; });
-let p5 = loadModel('./assets/star_wars_imperial_ii_star_destroyer/scene.gltf').then(result => {star_destroyer2 = result.scene.children[0]; });
-let p6 = loadModel('./assets/tatooine/scene.gltf').then(result => {tatooine = result.scene.children[0]; });
-let p7 = loadModel('./assets/sun/scene.gltf').then(result => {sun1 = result.scene.children[0]; });
-let p8 = loadModel('./assets/sun/scene.gltf').then(result => {sun2 = result.scene.children[0]; });
+let p1 = loadModel(
+  "./assets/star_wars_imperial_ii_star_destroyer/scene.gltf"
+).then((result) => {
+  star_destroyer1 = result.scene.children[0];
+});
+let p2 = loadModel(
+  "./assets/resistance_fleet__resistance_navy/scene.gltf"
+).then((result) => {
+  rebel_fleet = result.scene.children[0];
+});
+let p3 = loadModel("./assets/tie_fighter/scene.gltf").then((result) => {
+  tie_fighter = result.scene.children[0];
+});
+let p4 = loadModel("./assets/x-wing_fighter/scene.gltf").then((result) => {
+  x_wing = result.scene.children[0];
+});
+let p5 = loadModel(
+  "./assets/star_wars_imperial_ii_star_destroyer/scene.gltf"
+).then((result) => {
+  star_destroyer2 = result.scene.children[0];
+});
+let p6 = loadModel("./assets/tatooine/scene.gltf").then((result) => {
+  tatooine = result.scene.children[0];
+});
+let p7 = loadModel("./assets/sun/scene.gltf").then((result) => {
+  sun1 = result.scene.children[0];
+});
+let p8 = loadModel("./assets/sun/scene.gltf").then((result) => {
+  sun2 = result.scene.children[0];
+});
 
-Promise.all([p1, p2, p3, p4, p5, p6, p7, p8]).then( () => {
+Promise.all([p1, p2, p3, p4, p5, p6, p7, p8]).then(() => {
+  // Posición de los modelos
+  star_destroyer1.position.set(0, 0, 0);
+  star_destroyer2.position.set(-150, 0, 0);
+  rebel_fleet.position.set(0, 0, 500);
+  tatooine.position.set(9000, -2000, 0);
+  sun1.position.set(10000, 20000, -100000);
+  sun2.position.set(35000, 20000, -110000);
 
-	// Posición de los modelos
-	star_destroyer1.position.set(0, 0, 0);
-	star_destroyer2.position.set(-150, 0, 0);
-	rebel_fleet.position.set(0, 0, 500);
-	tatooine.position.set(9000, -2000, 0);
-	sun1.position.set(10000, 20000, -100000);
-	sun2.position.set(35000, 20000, -110000);
+  // Tamaño de los modelos
+  star_destroyer1.scale.set(0.5, 0.5, 0.5);
+  star_destroyer2.scale.set(0.3, 0.3, 0.3);
+  rebel_fleet.scale.set(0.00032, 0.00032, 0.00032);
+  tatooine.scale.set(7000, 7000, 7000);
+  sun1.scale.set(900, 900, 900);
+  sun2.scale.set(700, 700, 700);
 
-	// Tamaño de los modelos
-	star_destroyer1.scale.set(0.3, 0.3, 0.3);
-	star_destroyer2.scale.set(0.3, 0.3, 0.3);
-	rebel_fleet.scale.set(.00032, .00032, .00032);
-	tatooine.scale.set(7000, 7000, 7000);
-	sun1.scale.set(900, 900, 900);
-	sun2.scale.set(700, 700, 700);
+  tie_fighter.scale.set(1, 1, 1);
 
-	// Rotación inicial de los modelos
-	
+  //Rotación inicial de los modelos
+  const coords = { x: 0, y: 0, z: 0 };
+  const tween1 = new TWEEN.Tween(coords)
+    .to({ x: 100, y: 100, z: 100 })
+    .repeat(10)
+    .start();
 
-	// Agregar los modelos a la escena
-	scene.add(star_destroyer1);
-	// scene.add(star_destroyer2);
-	// scene.add(rebel_fleet);
-	scene.add(tatooine);
-	scene.add(sun1);
-	scene.add(sun2);
+  tween1.onUpdate(function (
+    obj = {
+      x,
+      y,
+      z,t
+    },
+    elapsed
+  ) {
+    tie_fighter.position.set(obj.x, obj.y, obj.z);
+  });
 
-	console.log("Entered promise");
-	animate();
-})
+  // Agregar los modelos a la escena
+  scene.add(star_destroyer1);
+  // scene.add(star_destroyer2);
+  // scene.add(rebel_fleet);
+  scene.add(tatooine);
+  scene.add(sun1);
+  scene.add(sun2);
+
+  scene.add(tie_fighter);
+
+  console.log("Entered promise");
+  animate();
+});
 
 // <----- RAF para animar todo ----->
 function animate() {
-  requestAnimationFrame(animate);
+  TWEEN.update();
   renderer.render(scene, camera);
+  requestAnimationFrame(animate);
 }
-animate();
